df3d0621c02c928494ead54dfba79f8a3ef37fe4
hl <zeldax54@gmail.com>
2017-12-02 14:42:47 -0500

    Primer commit

---
+++ src/Carteleria/CarteleriaBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Carteleria\CarteleriaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $c=$this->get('avanzu_admin_theme.context_helper');
+        $c->setOption('boxed_layout',true);
+        $c->setOption('skin','skin-green-light');
+        $c->setOption('control_sidebar',false);
+        $c->setOption('collapsed_sidebar',true);
+        return $this->render('CarteleriaCarteleriaBundle:Default:index.html.twig');
+    }
+}
